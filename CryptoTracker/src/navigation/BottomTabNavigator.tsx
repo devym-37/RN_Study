d04075b8d2cfd4a0f8e15d0bd0cd207679be2977
miniyoung <miniyoung37@gmail.com>
2022-02-08 21:48:56 +0900

    # CryptoTracker : add bottom navigator

---
+++ CryptoTracker/src/navigation/BottomTabNavigator.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Entypo, FontAwesome } from "@expo/vector-icons";
+import HomeScreen from "../screens/HomeScreen";
+import WatchlistScreen from "../screens/WatchlistScreen";
+
+const Tab = createBottomTabNavigator();
+
+const BottomTabNavigator = () => {
+    return (
+        <Tab.Navigator
+            initialRouteName='Home'
+            screenOptions={{
+                headerShown: false,
+                tabBarActiveTintColor: "white",
+                tabBarInactiveTintColor: "grey",
+                tabBarStyle: {
+                    backgroundColor: "#181818",
+                },
+            }}
+        >
+            <Tab.Screen
+                name='Home'
+                component={HomeScreen}
+                options={{
+                    tabBarIcon: ({ focused, color }) => <Entypo name='home' size={focused ? 30 : 25} color={color} />,
+                }}
+            />
+            <Tab.Screen
+                name='Watchlist'
+                component={WatchlistScreen}
+                options={{
+                    tabBarIcon: ({ focused, color }) => <FontAwesome name='star' size={focused ? 30 : 25} color={color} />,
+                }}
+            />
+        </Tab.Navigator>
+    );
+};
+
+export default BottomTabNavigator;
--- CryptoTracker/src/navigation/index.tsx
+++ CryptoTracker/src/navigation/index.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../screens/HomeScreen";
 import CoinDetailScreen from "../screens/CoinDetailScreen";
+import BottomTabNavigator from "./BottomTabNavigator";
 
 const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
     return (
-        <Stack.Navigator initialRouteName='' screenOptions={{ headerShown: false }}>
-            <Stack.Screen name={"Home"} component={HomeScreen} />
-            <Stack.Screen name={"CoinDetail"} component={CoinDetailScreen} />
+        <Stack.Navigator initialRouteName='Root' screenOptions={{ headerShown: false }}>
+            <Stack.Screen name='Root' component={BottomTabNavigator} />
+            <Stack.Screen name='CoinDetail' component={CoinDetailScreen} />
         </Stack.Navigator>
     );
 };
