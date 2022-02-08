7538be84e5e9d4e0244520443e1adcc73a6e9ce5
miniyoung <miniyoung37@gmail.com>
2022-02-08 21:49:16 +0900

    # CryptoTracker : add Home api

--- CryptoTracker/src/screens/HomeScreen/index.tsx
+++ CryptoTracker/src/screens/HomeScreen/index.tsx
@@ -1,12 +1,53 @@
-import React from "react";
-import { FlatList } from "react-native";
+import React, { useState, useEffect, useRef } from "react";
+import { ActivityIndicator, FlatList, RefreshControl } from "react-native";
 import CoinItem, { MarketCoinInfo } from "../../components/CoinItem";
 import cryptocurrencies from "../../../assets/data/cryptocurrencies.json";
+import { getMarketData } from "../../services/requests";
+
+const LIMIT_DATA = 50;
 
 const HomeScreen = () => {
+    const [coins, setCoins] = useState<any[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    const pageNo = useRef(1);
+
+    useEffect(() => {
+        fetchCoins(pageNo.current);
+    }, []);
+
+    const fetchCoins = async (pageNumber: number) => {
+        const coinsData = await getMarketData(pageNumber);
+        if (coinsData.length === LIMIT_DATA) {
+            pageNo.current += 1;
+        }
+        setCoins((prevState) => [...prevState, ...coinsData]);
+        setLoading(false);
+    };
+
+    const refetchCoins = async () => {
+        setLoading(true);
+        pageNo.current = 1;
+        const coinsData = await getMarketData(pageNo.current);
+        setCoins(coinsData);
+        setLoading(false);
+    };
+
     const renderItem = ({ item }: MarketCoinInfo) => <CoinItem marketCoin={item} />;
 
-    return <FlatList data={cryptocurrencies} renderItem={renderItem} keyExtractor={(item, index) => index.toString()} />;
+    if (loading || !coins) {
+        return <ActivityIndicator />;
+    }
+
+    return (
+        <FlatList
+            data={coins}
+            renderItem={renderItem}
+            keyExtractor={(item, index) => index.toString()}
+            refreshControl={<RefreshControl refreshing={loading} tintColor='white' onRefresh={refetchCoins} />}
+            onEndReached={() => fetchCoins(pageNo.current)}
+        />
+    );
 };
 
 export default HomeScreen;
---
+++ CryptoTracker/src/screens/WatchlistScreen/index.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+interface Props {}
+
+const WatchlistScreen: FC<Props> = () => {
+    return (
+        <View>
+            <Text>WatchlistScreen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default WatchlistScreen;
