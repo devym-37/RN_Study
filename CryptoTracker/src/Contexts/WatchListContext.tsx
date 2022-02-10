81642d092e2a630b80c43c2b9dd2cf35176e252d
miniyoung <miniyoung37@gmail.com>
2022-02-10 22:30:44 +0900

    # CryptoTracker : async-storage 함수 추가

--- CryptoTracker/src/Contexts/WatchListContext.tsx
+++ CryptoTracker/src/Contexts/WatchListContext.tsx
@@ -1,17 +1,50 @@
-import React, { useContext, createContext, useState } from "react";
+import React, { useContext, createContext, useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 interface Props {
     children: React.ReactElement;
 }
 
-const WatchListContext = createContext();
+const WatchListContext = createContext([]);
 
 export const useWatchList = () => useContext(WatchListContext);
 
 const WatchListProvider = ({ children }: Props) => {
-    const [watchListCoinIds, setWatchListCoinIds] = useState([]);
+    const [watchListCoinIds, setWatchListCoinIds] = useState<string[]>([]);
 
-    return <WatchListContext.Provider value={{ watchListCoinIds }}>{children}</WatchListContext.Provider>;
+    useEffect(() => {
+        getWatchListDate();
+    }, []);
+
+    const getWatchListDate = async () => {
+        try {
+            const response = await AsyncStorage.getItem("@watchList_coins");
+
+            setWatchListCoinIds(response !== null ? JSON.parse(response) : []);
+        } catch (e) {
+            console.log("getWatchListDate : ", e);
+        }
+    };
+
+    const saveWatchListCoinId = async (coinId: string) => {
+        try {
+            const newWatchList = [...watchListCoinIds, coinId];
+            const saveCoins = JSON.stringify("@watchList_coins", newWatchList);
+            await AsyncStorage.setItem("@storage_Key", saveCoins);
+            setWatchListCoinIds(newWatchList);
+        } catch (e) {
+            console.log("storeWatchListDate", e);
+        }
+    };
+
+    const removeWatchListCoinId = async (coinId: string) => {
+        const watchList = watchListCoinIds.filter((coin) => coin !== coinId);
+        const saveCoins = JSON.stringify("@watchList_coins", watchList);
+        await AsyncStorage.setItem("@storage_Key", saveCoins);
+        setWatchListCoinIds(watchList);
+    };
+
+    return <WatchListContext.Provider value={{ watchListCoinIds, saveWatchListCoinId, removeWatchListCoinId }}>{children}</WatchListContext.Provider>;
 };
 
 export default WatchListProvider;
