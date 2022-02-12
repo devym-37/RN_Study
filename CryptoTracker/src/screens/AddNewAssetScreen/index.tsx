d0d018b0168f210bc9ba283b337355eadb2d01be
miniyoung <miniyoung37@gmail.com>
2022-02-12 15:55:43 +0900

    # CryptoTracker : navigation Header style change

---
+++ CryptoTracker/src/screens/AddNewAssetScreen/index.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+interface Props {}
+
+const AddNewAssetScreen: FC<Props> = () => {
+    return (
+        <View>
+            <Text>AddNewAssetScreen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default AddNewAssetScreen;
