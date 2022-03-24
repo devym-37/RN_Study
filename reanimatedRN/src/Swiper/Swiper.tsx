d90aa58265c5ad52d4842722e68fdc3c8d8c3d8b
miniyoung <miniyoung37@gmail.com>
2022-03-24 23:08:44 +0900

    feat. swiper completed

--- reanimatedRN/src/Swiper/Swiper.tsx
+++ reanimatedRN/src/Swiper/Swiper.tsx
@@ -1,5 +1,20 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Image, StyleSheet, Dimensions } from "react-native";
+import { PanGestureHandler, State } from "react-native-gesture-handler";
+import Animated, {
+    add,
+    clockRunning,
+    cond,
+    debug,
+    divide,
+    eq,
+    floor,
+    not,
+    set,
+    useCode,
+    useValue,
+} from "react-native-reanimated";
+import { usePanGestureHandler, timing, snapPoint, useClock } from "react-native-redash/lib/module/v1";
 
 export const assets = [
     require("./assets/1.jpg"),
@@ -9,14 +24,68 @@ export const assets = [
     require("./assets/5.jpeg"),
 ];
 
+const { width, height } = Dimensions.get("window");
+
+const snapPoints = assets.map((_, i) => i * -width);
+
+const styles = StyleSheet.create({
+    container: {
+        ...StyleSheet.absoluteFillObject,
+        backgroundColor: "black",
+    },
+    pictures: {
+        width: width * assets.length,
+        height,
+        flexDirection: "row",
+    },
+    picture: {
+        width,
+        height,
+        overflow: "hidden",
+    },
+    image: {
+        ...StyleSheet.absoluteFillObject,
+        width: undefined,
+        height: undefined,
+    },
+});
+
 const Swiper = () => {
+    const clock = useClock();
+    const index = useValue(0);
+    const offsetX = useValue(0);
+    const translateX = useValue(0);
+    const { gestureHandler, state, velocity, translation } = usePanGestureHandler();
+
+    const to = snapPoint(translateX, velocity.x, snapPoints);
+
+    useCode(
+        () => [
+            cond(eq(state, State.ACTIVE), [set(translateX, add(offsetX, translation.x))]),
+            cond(eq(state, State.END), [
+                set(translateX, timing({ index, from: translateX, to })),
+                set(offsetX, translateX),
+                cond(not(clockRunning(clock)), [set(index, floor(divide(translateX, -width))), debug("index", index)]),
+            ]),
+        ],
+        []
+    );
+
     return (
-        <View>
-            <Text>Swiper</Text>
+        <View style={styles.container}>
+            <PanGestureHandler {...gestureHandler}>
+                <Animated.View style={StyleSheet.absoluteFill}>
+                    <Animated.View style={[styles.pictures, { transform: [{ translateX }] }]}>
+                        {assets.map((source) => (
+                            <View key={source} style={styles.picture}>
+                                <Image style={styles.image} {...{ source }} />
+                            </View>
+                        ))}
+                    </Animated.View>
+                </Animated.View>
+            </PanGestureHandler>
         </View>
     );
 };
-
-const styles = StyleSheet.create({});
 
 export default Swiper;
