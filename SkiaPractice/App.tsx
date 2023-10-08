8f31f64b35c0e3105765fc5c05df912b0af78a51
Lee Young Min <miniyoung37@gmail.com>
2023-10-08 22:57:24 +0900

    step2. Canvas context 추가

--- SkiaPractice/App.tsx
+++ SkiaPractice/App.tsx
@@ -7,25 +7,30 @@
 
 import React from 'react';
 import { SafeAreaView } from 'react-native';
-
+import { useContextBridge, FiberProvider } from 'its-fine';
 import { Canvas, Circle, Group } from '@shopify/react-native-skia';
 import CanvasPractice from './src/components/Canvas';
+import { ThemeProvider } from './src/components/Canvas/Theme/Theme';
 
 function App(): JSX.Element {
   const size = 256;
   const r = size * 0.33;
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      {/* <Canvas style={{ flex: 1 }}>
+    <FiberProvider>
+      <ThemeProvider primary="coral">
+        {/* <SafeAreaView style={{ flex: 1 }}> */}
+        {/* <Canvas style={{ flex: 1 }}>
         <Group blendMode="multiply">
           <Circle cx={r} cy={r} r={r} color="cyan" />
           <Circle cx={size - r} cy={r} r={r} color="magenta" />
           <Circle cx={size / 2} cy={size - r} r={r} color="yellow" />
         </Group>
       </Canvas> */}
-      <CanvasPractice />
-    </SafeAreaView>
+        <CanvasPractice />
+        {/* </SafeAreaView> */}
+      </ThemeProvider>
+    </FiberProvider>
   );
 }
 
--- SkiaPractice/src/components/Canvas/CanvasPractice.tsx
+++ SkiaPractice/src/components/Canvas/CanvasPractice.tsx
@@ -1,29 +1,21 @@
-import { useEffect } from 'react';
-import {
-  Canvas,
-  Image,
-  useCanvasRef,
-  Circle,
-} from '@shopify/react-native-skia';
+import { Canvas, Fill } from '@shopify/react-native-skia';
+import { useTheme } from './Theme/Theme';
+import { useContextBridge } from 'its-fine';
+
+const MyDrawing = () => {
+  const { primary } = useTheme();
+  return <Fill color={primary} />;
+};
 
 const CanvasPractice = () => {
-  const ref = useCanvasRef();
-  useEffect(() => {
-    setTimeout(() => {
-      // you can pass an optional rectangle
-      // to only save part of the image
-      const image = ref.current?.makeImageSnapshot();
-      if (image) {
-        // you can use image in an <Image> component
-        // Or save to file using encodeToBytes -> Uint8Array
-        const bytes = image.encodeToBytes();
-        console.log(bytes);
-      }
-    }, 1000);
-  });
+  const ContextBridge = useContextBridge();
+  // Fill component 후 순위가 높은 컴포넌트를 렌더링하면 덮어씌워짐
   return (
-    <Canvas style={{ flex: 1 }} ref={ref}>
-      <Circle r={128} cx={128} cy={128} color="red" />
+    <Canvas style={{ flex: 1 }}>
+      <ContextBridge>
+        <Fill color="black" />
+        <MyDrawing />
+      </ContextBridge>
     </Canvas>
   );
 };
